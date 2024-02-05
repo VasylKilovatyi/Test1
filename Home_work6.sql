e4340ce9d420c65bc26ef422632a535e2d26f676
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-02-05 18:27:49 +0200

    create table comment

--- Home_work6.sql
+++ Home_work6.sql
@@ -20,16 +20,44 @@ DELETE FROM users WHERE id > 4;
 -- Drop table
 DROP TABLE users;
 
-CREATE TABLE IF NOT EXISTS article (
+CREATE TABLE IF NOT EXISTS articles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT,
     user_id INTEGER CONSTRAINT fr_user_id REFERENCES users(id)
 );
-INSERT INTO article (title, content, user_id) VALUES ('Статя1', 'Текст1', 1);
-INSERT INTO article (title, content, user_id) VALUES ('Статя2', 'Текст2', 2);
-INSERT INTO article (title, content, user_id) VALUES ('Статя3', 'Текст3', 3);
+INSERT INTO articles (title, content, user_id) VALUES ('Статя1', 'Текст1', 1);
+INSERT INTO articles (title, content, user_id) VALUES ('Статя2', 'Текст2', 2);
+INSERT INTO articles (title, content, user_id) VALUES ('Статя3', 'Текст3', 3);
 
-SELECT * FROM article;
+SELECT * FROM articles;
 
-DROP TABLE article;
+DROP TABLE articles;
+
+CREATE TABLE IF NOT EXISTS comment (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    comment TEXT NOT NULL,
+    article_id INTEGER CONSTRAINT fr_article_id REFERENCES articles(id),
+    user_id INTEGER CONSTRAINT fr_user_id REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO comment (name, comment, article_id, user_id) VALUES ('Статя1', 'Текст1', 1, 2);
+INSERT INTO comment (name, comment, article_id, user_id) VALUES ('Статя2', 'Текст2', 2, 3);
+INSERT INTO comment (name, comment, article_id, user_id) VALUES ('Статя3', 'Текст3', 3, 1);
+
+
+SELECT * FROM comment;
+
+DROP TABLE comment;
+
+
+SELECT
+    comment.comment AS comment_content,
+    users.name AS user_name
+FROM
+    comment
+INNER JOIN
+    users ON comment.user_id = users.id;
+
