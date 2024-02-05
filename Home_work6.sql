393a011b424fd6d448f0d1cb6d57aff7e6905c81
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-02-05 16:29:15 +0200

    create table users and article

--- Home_work6.sql
+++ Home_work6.sql
@@ -1,19 +1,35 @@
 -- Active: 1706797900068@@127.0.0.1@5432@django_site_lesson@public
-  CREATE TABLE IF NOT EXISTS comment (
+CREATE TABLE IF NOT EXISTS public.users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
     
     
 );
 
-INSERT INTO comment (name) VALUES ('John');
-INSERT INTO comment (name) VALUES ('Jane');
-INSERT INTO comment (name) VALUES ('John');
+-- Insert data
+INSERT INTO users (name) VALUES ('Вася');
+INSERT INTO users (name) VALUES ('Влад');
+INSERT INTO users (name) VALUES ('Макс');
 
-SELECT * FROM comment;
+-- Select data
+SELECT * FROM users;
 
+-- Delete data
+DELETE FROM users WHERE id > 4;
 
--- delete 
-DELETE FROM comment WHERE id > 4;
+-- Drop table
+DROP TABLE users;
 
-DROP TABLE comment;
+CREATE TABLE IF NOT EXISTS article (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    user_id INTEGER CONSTRAINT fr_user_id REFERENCES users(id)
+);
+INSERT INTO article (title, content, user_id) VALUES ('Статя1', 'Текст1', 1);
+INSERT INTO article (title, content, user_id) VALUES ('Статя2', 'Текст2', 2);
+INSERT INTO article (title, content, user_id) VALUES ('Статя3', 'Текст3', 3);
+
+SELECT * FROM article;
+
+DROP TABLE article;
