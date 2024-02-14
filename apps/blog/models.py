dd8cb04d028a487bae762576b9101660d9dbce6d
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-02-14 16:21:13 +0200

    add articles

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=255)
+    content = models.TextField(verbose_name='Контент')
+    image = models.ImageField(verbose_name='Малюнок', upload_to='post_images/')
+    is_published = models.BooleanField(verbose_name='Опубліковано', default=False)
+    likes = models.IntegerField(verbose_name='Вподобайки', default=0)
+    views = models.IntegerField(verbose_name='Перегляди', default=0)
+    created_at = models.DateTimeField(verbose_name='Дата створення', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата оновлення', auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Пости'
+        ordering = ['-created_at']
+
+# Create your models here.
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.main',
     'apps.blog',
+    'apps.articles',
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sigmapythonhub",
+        "USER": "django_lesson",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
