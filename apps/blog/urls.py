db92e66d1b2667657075ed0782d84845fa2c13d4
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-02-15 17:17:53 +0200

    add post view

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.post, name='post'),
    
     
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-
+    
     context = {
         'posts': posts
     }
 
     return render(request, 'blog/index.html', context)
 
-# Create your views here.
+def post(request, post_id):
+    # post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blog/post.html', context)
