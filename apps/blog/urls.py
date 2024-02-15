eadb1c38fcce7ef9f228b218b98f0bc2b99f641f
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-02-15 19:11:19 +0200

    add form

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>/', views.post, name='post'),
+    path('create/', views.create, name='create'),
    
     
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
+
     posts = Post.objects.all()
-    
+    create_form = PostForm()
+
     context = {
-        'posts': posts
+        'posts': posts,
+        'form': create_form
     }
 
     return render(request, 'blog/index.html', context)
 
 def post(request, post_id):
     # post = Post.objects.get(id=post_id)
+
     post = get_object_or_404(Post, id=post_id)
     context = {
-        'post': post
+        'post': post,
     }
 
     return render(request, 'blog/post.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    return redirect('blog:index')
