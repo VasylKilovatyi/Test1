2918db7aa1f9b12b4e7581bebed89ae3fea26810
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-04-08 15:25:05 +0300

    add product Category view

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,15 +1,11 @@
 import uuid
-
 from django.contrib.admin import display
 from django.db import models
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-
 from mptt.models import MPTTModel, TreeForeignKey
-
 from imagekit.models import  ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill
-
 # Create your models here.
 class Catalog(MPTTModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -61,8 +57,8 @@ class Catalog(MPTTModel):
         verbose_name = 'Категорія'
         verbose_name_plural = 'Категорії'
         ordering = ['name']
-
-
+        
+        
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, verbose_name='Назва')
@@ -79,24 +75,24 @@ class Product(models.Model):
         verbose_name='Категорії',
         blank=True,
     )
-
-
+    
+    
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товари'
         ordering = ['-created_at']
-
-
+        
+        
     def __str__(self):
         return self.name
 
 
     def get_absolute_url(self):
-        return reverse("catalog:product", kwargs={"slug": self.slug})
+        return reverse("catalog:product", kwargs={"category_slug": self.main_category().slug, "product_slug": self.slug})
 
     def images(self):
         return Image.objects.filter(product=self.id)
-
+    
     def main_image(self):
         image = Image.objects.filter(product=self.id, is_main=True).first()
         if image:
@@ -104,41 +100,41 @@ class Product(models.Model):
         return self.images().first()
 
     def main_category(self):
-        category = self.category.filter(category__productcategory__is_main=True).first() #вибираємо категорію, яка є основною
+        category = self.category.filter(productcategory__is_main=True).first()
+        print(category)
         if category:
             return category
         return self.category.first()
-
+    
     @display(description='Ціна')
     def price_display(self):
         return f'{self.price} грн.'
-
+    
     @display(description='Основне зображення')
     def image_tag(self):
         image = self.main_image()
         if image:
             return mark_safe(f'<img src="{image.image_thumbnail.url}" />')
-
-
-
+    
+    
 class ProductCategory(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
     category = models.ForeignKey(Catalog, on_delete=models.CASCADE, verbose_name='Категорія')
     is_main = models.BooleanField(default=False, verbose_name='Основна категорія')
-
+    
     def __str__(self):
         return f'{self.product.name} -> {self.category.name}'
-
+    
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if self.is_main:
             ProductCategory.objects.filter(product=self.product, is_main=True).update(is_main=False)
         super().save(force_insert, force_update, using, update_fields)
-
+        
     class Meta:
         verbose_name = 'Категорія товару'
         verbose_name_plural = 'Категорії товарів'
-
-
+        
+        
 class Image(models.Model):
     image = ProcessedImageField(
         verbose_name='Зображення',
@@ -162,13 +158,13 @@ class Image(models.Model):
         verbose_name='Товар'
     )
     is_main = models.BooleanField(default=False, verbose_name='Основне зображення')
-
-
+    
+    
     @display(description='Зображення')
     def image_tag_thumbnail(self):
         if self.image:
             return mark_safe(f'<img src="{self.image_thumbnail.url}" height="70" />')
-
+        
     @display(description='Основне зображення')
     def image_tag(self):
         if self.image:
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,11 +1,34 @@
 from django.views.generic import ListView, DetailView
-from .models import Catalog
+from .models import Catalog, Product
+
 # Create your views here.
 
 class CataloglistView(ListView):
     model = Catalog
     template_name = 'catalog/index.html'
     context_object_name = 'categories'
-    # paginate_by = 3
 
-# Create your views here.
+
+    def get_queryset(self):
+        return Catalog.objects.filter(parent=None)
+
+
+class ProductByCategoryView(ListView):
+    model = Catalog
+    template_name = 'catalog/product_by_category.html'
+    context_object_name = 'category'
+
+    def get_queryset(self):
+        self.category = Catalog.objects.get(slug=self.kwargs['slug'])
+        self.categories = Catalog.objects.filter(parent=self.category)
+        self.all_categories = self.categories.get_descendants(include_self=True)
+        print(self.all_categories)
+        queryset = Product.objects.filter(productcategory__category__in=self.all_categories)
+        print(queryset)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        context['categories'] = self.categories
+        context['category'] = self.category
+        return context
