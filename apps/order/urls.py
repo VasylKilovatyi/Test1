cfd11f65e2c2c13a4a0570299b79229121bce742
Василь Кіловатий <kilovatyivasyl1@gmail.com>
2024-04-15 18:23:42 +0300

    Add total_price method to Cart model and create CartView

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AddToCartView
+from .views import AddToCartView, CartView
 
 app_name = 'order'
 
 urlpatterns = [
+    path('', CartView.as_view(), name='cart'),
     path('add/', AddToCartView.as_view(), name='add_to_cart'),
 ]
